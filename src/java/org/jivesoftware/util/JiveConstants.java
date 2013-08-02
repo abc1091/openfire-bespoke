b27996b4e1878326ca40ba25f54a82ccc7672259
Ash Ward <ash.wardly@gmail.com>
2013-08-02 15:41:11 +0100

    Initial import of vanilla Openfire 3.8.0 code

---
+++ src/java/org/jivesoftware/util/JiveConstants.java
@@ -0,0 +1,40 @@
+/**
+ * $RCSfile$
+ * $Revision: 13437 $
+ * $Date: 2013-02-04 13:34:30 +0000 (Mon, 04 Feb 2013) $
+ *
+ * Copyright (C) 2004-2008 Jive Software. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jivesoftware.util;
+
+/**
+ * Contains constant values representing various objects in Jive.
+ */
+public class JiveConstants {
+
+    public static final int SYSTEM = 17;
+    public static final int ROSTER = 18;
+    public static final int OFFLINE = 19;
+    public static final int MUC_ROOM = 23;
+    public static final int SECURITY_AUDIT = 25;
+    public static final int MUC_SERVICE = 26;
+
+    public static final long SECOND = 1000;
+    public static final long MINUTE = 60 * SECOND;
+    public static final long HOUR = 60 * MINUTE;
+    public static final long DAY = 24 * HOUR;
+    public static final long WEEK = 7 * DAY;
+}
